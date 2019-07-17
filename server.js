2d2b586a43c0eae06c2d8ee9b991ba8f0cd8906d
Deivson Rayner <deivsonrayner@gmail.com>
2019-07-17 17:01:07 -0300

    melhorias

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require('appmetrics-prometheus').attach();
 require('dotenv').config({silent: true, path: `${__dirname}/.env`});
 
 var server = require('./app');
-var port = 80;
+var port = 8080;
 
 console.log(`Running on ${process.env.BASE_PATH}:${port}`)
 
